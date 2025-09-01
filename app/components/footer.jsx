80a5e13671f58db666acff82203b8f36f12c9cfe
Arjay <133982801+Arjay1311@users.noreply.github.com>
2025-09-01 18:18:31 +0530

    Update footer.jsx

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="footer width-100 border z-10 border-t-[#33353F] border-b-black border-l-transparent border-r-transparent text-white">
       <div className="w-full p-6 flex justify-between">
-        <Image src="/images/logo.png" alt="Logo" height={50} width={50} />
+        <Image src="/images/Logo.png" alt="Logo" height={50} width={50} />
         <Link href={"/"} className="mt-4 text-purple-400 hover:underline">
           Back to Top
         </Link>
